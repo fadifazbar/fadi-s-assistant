86ad0233e9d02603cff83042068410540f1b5878
fadifazbar <fadifazbar90@gmail.com>
2025-09-27 21:05:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class ModBot(commands.Bot):
 
         await self.change_presence(
             activity=discord.Activity(
-                type=discord.ActivityType.costum,
+                type=discord.ActivityType.custom,
                 name=f"🤩 Use {Config.DEFAULT_PREFIX}help | Moderation And Fun Bot :p"
             ),
             status=discord.Status.online
