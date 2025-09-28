5274632644fab78e6fdddc8d5ee3cf955f4c942d
fadifazbar <fadifazbar90@gmail.com>
2025-09-28 09:40:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,9 +50,6 @@ class ModBot(commands.Bot):
 
         logger.info("✅ Loaded cogs (slash commands will now auto-sync)")
 
-        # --- Global slash command sync ---
-        synced = await self.tree.sync()  # Global sync
-        logger.info(f"🎯 Synced {len(synced)} Commands! ✅")
 
     async def on_ready(self):
         logger.info(f"✅ Bot is ready! Logged in as {self.user}")
