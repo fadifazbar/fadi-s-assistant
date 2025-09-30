d167ea1148dfc12500c6baa2ef10006785b99eaa
fadifazbar <fadifazbar90@gmail.com>
2025-09-30 19:55:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ class ModBot(commands.Bot):
 
         # Load all cogs
         await self.load_extension("cogs.fix")
+        await self.load_extension("cogs.skibidibattle")
         await self.load_extension("cogs.warning")
         await self.load_extension("cogs.moderation")
         await self.load_extension("cogs.general")
