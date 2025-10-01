967f460fd2a498e744d82263bf6797830fa7ff09
fadifazbar <fadifazbar90@gmail.com>
2025-10-01 10:25:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class ModBot(commands.Bot):
         logger.info("Setting up bot...")
 
         # Load all cogs
+        await self.load_extension("cogs.autorole")
         await self.load_extension("cogs.fix")
         await self.load_extension("cogs.skibidibattle")
         await self.load_extension("cogs.warning")
