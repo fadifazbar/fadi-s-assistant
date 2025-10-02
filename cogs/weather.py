03f559bcdee54cfbda742200db2dba516b6bdf9d
fadifazbar <fadifazbar90@gmail.com>
2025-10-02 05:30:02 +0100

    Update weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 
-API_KEY = is.getinv("WEATHER")  # Replace with your actual key
+API_KEY = os.getenv("WEATHER")  # Replace with your actual key
 
 class Weather(commands.Cog):
     def __init__(self, bot):
